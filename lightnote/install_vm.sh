6ee6e9e354651b6337a8a6c37c8655ae0018a909
Mezentsev Denis <mezentsev.d@olytechspb.com>
2024-10-26 22:58:32 +0300

    init

--- lightnote/install_vm.sh
+++ lightnote/install_vm.sh
@@ -2,6 +2,6 @@
 
 sudo nix --experimental-features "nix-command flakes" run github:nix-community/disko/latest -- --mode disko disko.nix
 sudo nixos-generate-config --root /mnt
-sudo cp configuration_headless.nix /mnt/etc/nixos/configuration.nix
+sudo cp configuration.nix /mnt/etc/nixos/configuration.nix
 sudo nixos-install
 sudo nixos-enter --root /mnt -c 'passwd mezlogo'
---
+++ vm/configuration_headless.nix
@@ -0,0 +1,37 @@
+{ config, lib, pkgs, ... }: {
+  imports =
+    [
+      ./hardware-configuration.nix
+    ];
+
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  networking.hostName = "nixvm";
+  networking.networkmanager.enable = true;
+
+  time.timeZone = "Europe/Moscow";
+
+  i18n = {
+    defaultLocale = "en_US.UTF-8";
+    supportedLocales = [
+      "en_US.UTF-8"
+      "ru_RU.UTF-8"
+    ];
+  }
+
+  users.users.alice = {
+    isNormalUser = true;
+    extraGroups = [ "wheel" "networkmanager" "input" ];
+  };
+
+  environment.systemPackages = with pkgs; [
+    neovim
+    tree
+  ];
+
+  services.openssh.enable = true;
+  networking.firewall.enable = false;
+
+  system.stateVersion = "24.05";
+}
